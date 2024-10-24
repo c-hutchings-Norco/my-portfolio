acfb4825fa2590f5b80730530a36e92597b9a142
c-hutchings-Norco <139173525+c-hutchings-Norco@users.noreply.github.com>
2024-10-23 23:23:12 -0700

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <h1>Caroline Hutchings</h1>
+    <p>My name is Caroline and I teach math and data science at Norco College. I am passionate about teaching and data science!</p>
+    <img 
+        class="fit-picture"
+        src="https://raw.githubusercontent.com/c-hutchings-Norco/Portfolio/main/images/Denmark.jpg"
+        alt="Caroline Hutchings with family in Denmark">
+    <p>I went to Denmark this summer to watch one of my favorite players!</p>
+</main> 
