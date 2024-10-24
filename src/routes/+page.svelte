9cacea24d62231287ee9a7323ba0869ff5964b0c
c-hutchings-Norco <139173525+c-hutchings-Norco@users.noreply.github.com>
2024-10-23 23:43:38 -0700

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     <p>My name is Caroline and I teach math and data science at Norco College. I am passionate about teaching and data science!</p>
     <img 
         class="fit-picture"
-        src="https://raw.githubusercontent.com/c-hutchings-Norco/Portfolio/main/images/Denmark.jpg"
+        src="/images/Denmark.jpg" 
         alt="Caroline Hutchings with family in Denmark">
     <p>I went to Denmark this summer to watch one of my favorite players!</p>
 </main> 
