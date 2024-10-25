6d12d8775453e752af1bdfa46241d0a019e0d560
c-hutchings-Norco <139173525+c-hutchings-Norco@users.noreply.github.com>
2024-10-25 11:32:11 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<main>
-    <h1>Caroline Hutchings</h1>
-    <p>My name is Caroline and I teach math and data science at Norco College. I am passionate about teaching and data science!</p>
-    <img 
-        class="fit-picture"
-        src="images/Denmark.jpg" 
-        alt="Caroline Hutchings with family in Denmark">
-    <p>I went to Denmark this summer to watch one of my favorite players! I loved Denmark!!</p>
-</main> 
+<script>
+    import Project from '$lib/Project.svelte';
+    import projects from '$lib/projects.json';
+    </script>
+    
+    <main>
+        <h1>Caroline Hutchings</h1>
+        <p>My name is Caroline and I teach math and data science at Norco College. I am passionate about teaching and data science!</p>
+        <img 
+            class="fit-picture"
+            src="images/Denmark.jpg" 
+            alt="Caroline Hutchings with family in Denmark">
+        <p>I went to Denmark this summer to watch one of my favorite players! I loved Denmark!!</p>
+    
+        <h2>Latest Projects</h2>
+        <div class="projects highlights">
+            {#each projects.slice(0, 3) as project}
+                <Project data={project} hLevel={3} />
+            {/each}
+        </div>
+    </main>
