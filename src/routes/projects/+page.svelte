bc94a9107e7851615a247184652e39a21d5e3da3
c-hutchings-Norco <139173525+c-hutchings-Norco@users.noreply.github.com>
2024-10-25 11:27:14 -0700

    update

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,15 +1,12 @@
 <script>
+    import Project from '$lib/Project.svelte';
     import projects from '$lib/projects.json';
     </script>
     
     <h1>My Projects ({projects.length})</h1>
     
     <div class="projects">
-      {#each projects as p}
-        <article>
-          <h2>{p.title}</h2>
-          <img src={p.image} alt={p.title} />
-          <p>{p.description}</p>
-        </article>
+      {#each projects as p} 
+        <Project data = {p} />
       {/each}
     </div>
