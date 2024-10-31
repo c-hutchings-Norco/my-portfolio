9b5a37ef64e04e5249c69fa76f890d40d681f63a
kodaoluidris <kodaoluidris@gmail.com>
2024-10-31 04:12:28 +0100

    Fixed duplicate navbar and passing current class on the active tab, as well export the theme switcher to a separate component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,24 @@
 <script>
   import { page } from '$app/stores';
   import { base } from '$app/paths';
-  import { onMount } from 'svelte';
+  import ThemeSwitcher from '../components/ThemeSwitcher.svelte';
+  import DataFetcher from '../components/DataFetcher.svelte';
 
   const pages = [
     { url: '/', title: 'Home' },
-    { url: 'projects', title: 'Projects' },
-    { url: 'contact', title: 'Contact' },
-    { url: 'cv', title: 'CV/Resume' },
+    { url: '/projects', title: 'Projects' },
+    { url: '/contact', title: 'Contact' },
+    { url: '/cv', title: 'CV/Resume' },
     { url: 'https://github.com/c-hutchings-Norco', title: 'GitHub' },
   ];
 
-  let theme = 'light dark';
-
-  function setColorScheme(colorScheme) {
-    if (typeof document !== 'undefined') {
-      document.documentElement.style.setProperty('color-scheme', colorScheme);
-      if (colorScheme === "dark") {
-        document.body.classList.add("dark-mode");
-      } else {
-        document.body.classList.remove("dark-mode");
-      }
-      localStorage.colorScheme = colorScheme;
-    }
-  }
-
-  onMount(() => {
-    if ("colorScheme" in localStorage) {
-      theme = localStorage.colorScheme;
-      setColorScheme(theme);
-    } else if (typeof window !== 'undefined') {
-      const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-      setColorScheme(prefersDarkScheme ? "dark" : "light");
-    }
-  });
 </script>
 
 <nav>
   {#each pages as p}
     <a
       href={p.url.startsWith('http') ? p.url : `${base}${p.url}`}
-      class:current={$page.url.pathname === `${base}${p.url}`}
+      class:current={$page?.url?.pathname === `${base}${p.url}`}
       target={p.url.startsWith('http') ? '_blank' : undefined}
       rel={p.url.startsWith('http') ? 'noopener noreferrer' : undefined}
     >
@@ -49,15 +27,10 @@
   {/each}
 </nav>
 
-<label class="color-scheme">
-  Theme:
-  <select bind:value={theme} on:change={(e) => setColorScheme(e.target.value)}>
-    <option value="light dark">Automatic</option>
-    <option value="light">Light</option>
-    <option value="dark">Dark</option>
-  </select>
-</label>
+
 
 <main>
+  <ThemeSwitcher />
   <slot />
+  <DataFetcher />
 </main>
