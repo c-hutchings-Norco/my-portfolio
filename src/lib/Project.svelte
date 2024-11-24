641d4533f510e24229795e0d1885288b87532129
Caroline Hutchings <139173525+c-hutchings-Norco@users.noreply.github.com>
2024-11-23 22:05:05 -0800

    Update Project.svelte

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,15 +1,16 @@
 
-<script>
+  <script>
   export let data = {};
   export let hLevel = 2;
 </script>
-  
-  <article>
-    <svelte:element this={'h' + hLevel}>{data.title}</svelte:element>
-    <img src={data.image} alt={data.title} />
-    <p>{data.description}</p>
-    <span>{data.year}</span>
-  </article>
-  
-  
+
+<article>
+  <svelte:element this={'h' + hLevel}>{data.title}</svelte:element>
+  <img src={data.image} alt={data.title} />
+  <p>{data.description}</p>
+  <span>{data.year}</span>
+  {#if data.url}
+    <a href={data.url} target="_blank" rel="noopener noreferrer">View Project</a>
+  {/if}
+</article>
     
